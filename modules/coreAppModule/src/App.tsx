7c3437b5e3aa923dfc93fa9a6d857026c6eb8f29
Alexandr Samsonov <aleksandr.samsonov@cosysoft.ru>
2021-05-19 17:37:13 +0400

    add declare types for import remote module

--- modules/coreAppModule/src/App.tsx
+++ modules/coreAppModule/src/App.tsx
@@ -2,11 +2,8 @@ import React, { StrictMode, Suspense } from 'react';
 import { Typography } from '@material-ui/core';
 import { makeStyles, Theme } from '@material-ui/core';
 
-// @ts-ignore
 const ThemeProvider = React.lazy(() => import('uiDesignModule/uiDesignTheme'));
-// @ts-ignore
 const Button = React.lazy(() => import('uiDesignModule/uiDesignElements/Button'));
-// @ts-ignore
 const Auth = React.lazy(() => import('authorizationModule/Auth'));
 
 export const useStyles = makeStyles<Theme>((theme: Theme) => ({
---
+++ modules/coreAppModule/src/coreAppModule.d.ts
@@ -0,0 +1,14 @@
+declare module 'uiDesignModule/uiDesignTheme' {
+    const ThemeProvider: React.ComponentType
+    export default ThemeProvider
+}
+
+declare module 'uiDesignModule/uiDesignElements/Button' {
+    const Button: React.ComponentType
+    export default Button
+}
+
+declare module 'authorizationModule/Auth' {
+    const Auth: React.ComponentType
+    export default Auth
+}
